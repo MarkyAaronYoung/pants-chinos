bf79de4ff098e7de1b3bb54313cb8fa066972724
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-11 21:48:13 -0500

    added future array

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('main things')
+
+//Future Page array
+const futureCards = [
+  {
+    image: images/paris.png,
+    location: Paris,
+    date: 1/20/21 - 1/23/21
+  },
+  {
+    image: images/newyork.png,
+    location: New York,
+    date: 2/6/21 - 2/13/21
+  },
+  {
+    image: images/london.png,
+    location: London,
+    date: 2/14/21 - 2/18/21
+  },
+  {
+    image: images/nashville.png,
+    location: Nashville,
+    date: 2/24/21 - 3/4/21
+  }
+]
