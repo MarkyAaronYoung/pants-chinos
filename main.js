258e7d495d2528a7acda514cda7d5ead4106d39f
Brooke Nemchak <bnemchak@gmail.com>
2020-05-11 21:20:03 -0500

    WIP - content

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 console.log('main things')
+
+// Past Array
+
+const pastCard = [
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+  
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+
+  {
+    imageUrl: ,
+    name: ,
+    date: ,
+    country: ,
+  },
+]
