ac35d6fbbe6767294bc31b80305857e06bff4319
Geno McNew <genemcnew3@gmail.com>
2020-05-12 20:44:47 -0500

    fixed content syntax

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 //Future(Fashion Week) Page array
 futureCards = [
   {
-    image: images/paris.png,
+    image: 'images/paris.png',
     location: "Paris",
     date: "1/20/21 - 1/23/21"
   },
   {
-    image: images/newyork.png,
+    image: 'images/newyork.png',
     location: "New York",
     date: "2/6/21 - 2/13/21"
   },
   {
-    image: images/london.png,
+    image: 'images/london.png',
     location: "London",
     date: "2/14/21 - 2/18/21"
   },
   {
-    image: images/nashville.png,
+    image: 'images/nashville.png',
     location: "Nashville",
     date: "2/24/21 - 3/4/21"
   }
